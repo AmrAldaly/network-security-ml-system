d5cfce92593ec6e6173037af71543bf22b2691bf
AmrAlaly <amraldaly4242@gmail.com>
2026-02-20 08:07:47 +0200

    Data Validation Done

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from src.components.data_ingestion import DataIngestion
 from src.exception.exception import CustomException
 from src.logging.logger import logging
-from src.entity.config_entity import DataIngestionConfig
+from src.entity.config_entity import DataIngestionConfig,DataValidationConfig
 from src.entity.config_entity import TrainingPipelineConfig
+from src.components.data_validation import DataValidation
 
 
 import sys
@@ -16,6 +17,12 @@ if __name__=='__main__':
         dataingestionartifact=data_ingestion.initiate_data_ingestion()
         logging.info("Data Initiation Completed")
         print(dataingestionartifact)
+        data_validation_config=DataValidationConfig(trainingpipelineconfig)
+        data_validation=DataValidation(dataingestionartifact,data_validation_config)
+        logging.info("Initiate the data Validation")
+        data_validation_artifact=data_validation.initiate_data_validation()
+        logging.info("data Validation Completed")
+        print(data_validation_artifact)
     
     except Exception as e:
         raise CustomException(e,sys)
